aaddbfd76647915ddd8f77cf3c8384139913050c
Pete Lambert <pete@petelambert.com>
2022-12-31 18:04:23 +0000

    Removed Vite. It was causing problems. Revert to 11ty's new dev server

--- .eleventy.js
+++ .eleventy.js
@@ -4,9 +4,6 @@ const markdownItAnchor = require('markdown-it-anchor');
 const EleventyPluginNavigation = require('@11ty/eleventy-navigation');
 const EleventyPluginRss = require('@11ty/eleventy-plugin-rss');
 const EleventyPluginSyntaxhighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
-const EleventyVitePlugin = require('@11ty/eleventy-plugin-vite');
-
-const rollupPluginCritical = require('rollup-plugin-critical').default;
 const readingTime = require('eleventy-plugin-reading-time');
 
 const filters = require('./utils/filters.js');
@@ -21,67 +18,38 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(EleventyPluginNavigation);
   eleventyConfig.addPlugin(EleventyPluginRss);
   eleventyConfig.addPlugin(EleventyPluginSyntaxhighlight);
-  eleventyConfig.addPlugin(EleventyVitePlugin, {
-    tempFolderName: '.11ty-vite', // Default name of the temp folder
+  eleventyConfig.setServerOptions({
+    // Default values are shown:
 
-    // Vite options (equal to vite.config.js inside project root)
-    viteOptions: {
-      publicDir: 'public',
-      clearScreen: false,
-      server: {
-        mode: 'development',
-        middlewareMode: true,
-      },
-      appType: 'custom',
-      assetsInclude: ['**/*.xml', '**/*.txt'],
-      build: {
-        mode: 'production',
-        sourcemap: 'true',
-        manifest: true,
-        // This puts CSS and JS in subfolders – remove if you want all of it to be in /assets instead
-        rollupOptions: {
-          output: {
-            assetFileNames: 'assets/css/main.[hash].css',
-            chunkFileNames: 'assets/js/[name].[hash].js',
-            entryFileNames: 'assets/js/[name].[hash].js',
-          },
-          plugins: [
-            rollupPluginCritical({
-              criticalUrl: './build/',
-              criticalBase: './build/',
-              criticalPages: [
-                { uri: 'index.html', template: 'index' },
-                { uri: 'journal/index.html', template: 'journal/index' },
-                { uri: '404.html', template: '404' },
-              ],
-              criticalConfig: {
-                inline: true,
-                dimensions: [
-                  {
-                    height: 900,
-                    width: 375,
-                  },
-                  {
-                    height: 720,
-                    width: 1280,
-                  },
-                  {
-                    height: 1080,
-                    width: 1920,
-                  },
-                ],
-                penthouse: {
-                  forceInclude: [
-                    '.fonts-loaded-1 body',
-                    '.fonts-loaded-2 body',
-                  ],
-                },
-              },
-            }),
-          ],
-        },
-      },
+    // Opt-out of the live reload snippet
+    enabled: true,
+
+    // Opt-out of DOM diffing updates and use page reloads
+    domdiff: true,
+
+    // The starting port number to attempt to use
+    port: 8080,
+
+    // number of times to increment the port if in use
+    portReassignmentRetryCount: 10,
+
+    // Show local network IP addresses for device testing
+    showAllHosts: false,
+
+    // Use a local key/certificate to opt-in to local HTTP/2 with https
+    https: {
+      // key: "./localhost.key",
+      // cert: "./localhost.cert",
     },
+
+    // Change the name of the special folder name used for injected scripts
+    folder: '.11ty',
+
+    // Show the server version number on the command line
+    showVersion: false,
+
+    // Change the default file encoding for reading/serving files
+    encoding: 'utf-8',
   });
 
   // Filters
